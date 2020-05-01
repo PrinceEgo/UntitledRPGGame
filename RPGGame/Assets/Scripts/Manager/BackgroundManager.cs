97d918174c6575b5a319d244552afb5594bc70f8
Mallows <barino.jm@gmail.com>
2020-05-02 00:17:57 +0800

    Update for background, camera
    - Background
    - Camera

---
+++ RPGGame/Assets/Scripts/Manager/BackgroundManager.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundManager : MonoBehaviour
+{
+    public List<BackgroundLayers> layers = new List<BackgroundLayers>();
+
+    private void Start()
+    {
+        CameraManager.Instance.OnScrolling.AddListener(HandleOnScrolling);
+    }
+
+    private void HandleOnScrolling(float scrollValue)
+    {
+        foreach (var layer in layers)
+        {
+            layer.Scroll(scrollValue);
+        }
+    }
+}
+
+[System.Serializable]
+public class BackgroundLayers
+{
+    public Transform obj = null;
+    [Range(0, 10)]
+    public float scrollSpeed = 1f;
+
+    public void Scroll(float scrollValue)
+    {
+        obj.transform.position = new Vector3(-(scrollValue * scrollSpeed), obj.transform.position.y, obj.transform.position.z);
+    }
+}
---
+++ RPGGame/Assets/Scripts/Manager/CameraManager.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class CameraManager : MonoBehaviour
+{
+    public static CameraManager Instance { private set; get; }
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            Destroy(this);
+        }
+    }
+
+    public Transform target = null;
+    [Range(0f, 1f)]
+    public float smoothing = 0.2f;
+
+    public FloatEvent OnScrolling = new FloatEvent();
+
+    Vector3 refVelocity;
+
+    private void FixedUpdate()
+    {
+        Follow();
+    }
+    private void Update()
+    {
+        Scrolling();
+    }
+
+    void Follow()
+    {
+        Vector3 targetPos = new Vector3(target.position.x, transform.position.y, transform.position.z);
+        //transform.position = Vector3.Lerp(transform.position, targetPos, smoothing);
+        transform.position = Vector3.SmoothDamp(transform.position, targetPos, ref refVelocity, smoothing);
+    }
+
+    void Scrolling()
+    {
+        OnScrolling.Invoke(transform.position.x);
+    }
+}
+
+public class FloatEvent : UnityEvent<float> { }
